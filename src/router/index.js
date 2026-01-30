e3df4eb2d45ac5eab62ec7e960fbcc51870bb56a
mochiemi <tiemi.suyama@gmail.com>
2026-01-30 08:35:25 -0300

    config router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
         {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+        {
       path: '/medicaments',
       name: 'medicamentos',
       component: () => import('../views/MedicamentsView.vue')
